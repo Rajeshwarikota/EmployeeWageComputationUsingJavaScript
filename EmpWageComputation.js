b0f2dd61301a7fc015339ea101fc4559524a7280
Rajeshwarikota <kotarajeswari003.com>
2023-04-09 22:55:41 +0530

    [Rajeshwari kota] Adding Employee Attendance

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,1 +1,11 @@
 console.log("....EmpWageComputation....")
+const Is_Present =0;
+let EmpCheck = Math.floor(Math.random()*10) % 2
+if (EmpCheck == Is_Present)
+{
+    console.log("Employee is Present");
+}
+else
+{
+    console.log("Employee is Absent");
+}
